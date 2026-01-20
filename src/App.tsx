52a51798011770405da1d12dbb30aec3f5ab4029
mkudmi <mku.dmi@gmail.com>
2026-01-20 22:36:48 +0300

    TODO

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,15 @@ import type { RequestDraft, RequestHistoryItem } from './shared/types/requestHis
 import { appendRequestHistoryItem, loadRequestHistoryByRequestId, saveRequestHistoryByRequestId } from './shared/utils/requestHistory'
 import { syncCollectionKeepingIds } from './shared/utils/syncCollection'
 
-//TODO: поравить отображение хэдеров в редакторе, сделать key глобального хэдера редактируемым, добавить чек-бокс активный или неактивный хэдер, пре-пост скрипты, история поиска, запаковать все в exe
+//TODO: 
+// параметр игнорирования ssl сертификатов
+// поравить отображение хэдеров в редакторе
+// сделать key глобального хэдера редактируемым
+// добавить чек-бокс активный или неактивный хэдер
+// пре-пост скрипты
+// история поиска
+// поправить юай для ноутбуков и fullhd
+// запаковать все в exe
 
 function isAbsoluteUrl(url: string) {
   return /^[a-zA-Z][a-zA-Z0-9+.-]*:\/\//.test(url) || url.startsWith('//')
