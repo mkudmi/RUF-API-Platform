147f469927c9c87bf996af2dbcaf88276928f50d
mkudmi <mku.dmi@gmail.com>
2026-02-02 19:54:11 +0300

    choose ruf collection fix

--- src/modules/import/components/ImportFab.tsx
+++ src/modules/import/components/ImportFab.tsx
@@ -167,7 +167,7 @@ export function ImportFab(props: {
       <input
         ref={fileInputRef}
         type="file"
-        accept=".json,.yaml,.yml,.wsdl,.xml"
+        accept=".rufcollection,.json,.yaml,.yml,.wsdl,.xml"
         style={{ display: 'none' }}
         onChange={onFileSelected}
       />
